3c7a4bbf093b872c5969ffd251b69578b15760db
saidolim10 <worksaidolim@gmail.com>
2025-06-10 12:34:09 +0500

    togilanga

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Header from './components/Header'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
-    App page hush kelibsiz
+    
 
-<Header/>
+<Home/>
 
-    <Link to={"/About"}>About pageg otisgh</Link>
+    
     </div>
   )
 }
