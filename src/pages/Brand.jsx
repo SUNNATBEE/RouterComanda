8c53fe05c5ba000c3e4b3eeec748036cd35f4779
saidolim10 <worksaidolim@gmail.com>
2025-06-10 13:58:44 +0500

    HEader tugadi

--- src/pages/Brand.jsx
+++ src/pages/Brand.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Brand = () => {
   return (
     <div>
+      <Header/>
         Brand Page
 
         <h2>Azim Page</h2>
--- src/pages/Third.jsx
+++ src/pages/Third.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Third = () => {
   return (
     <div>
+      <Header/>
         Преимущества
 
         <h2>Siroj vs Fayzullo</h2>
